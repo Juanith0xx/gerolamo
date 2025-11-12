67c663e347aba527ad2b4c94569a5ddb8aa6c14d
Juan Estay <juan.estay.rodriguez@icloud.com>
2025-11-11 22:28:37 -0300

    efecto calculadora v1

--- src/components/FeedingCalculator/StepTracker.jsx
+++ src/components/FeedingCalculator/StepTracker.jsx
@@ -19,19 +19,19 @@ export default function StepTracker({ step, petName = "tu mascota" }) {
               {/* Línea conectora */}
               {index !== 0 && (
                 <div
-                  className={`absolute top-4 left-[-50%] w-full h-[2px] ${
-                    isCompleted ? "bg-red-500" : "bg-gray-300"
+                  className={`absolute top-4 left-[-50%] w-full h-[8px] ${
+                    isCompleted ? "bg-[#417ABD]" : "bg-gray-300"
                   }`}
                 ></div>
               )}
 
               {/* Círculo del paso */}
               <div
-                className={`w-6 h-6 flex items-center justify-center rounded-full z-10 transition-all duration-300 ${
+                className={`w-12 h-12 flex items-center justify-center rounded-full z-10 transition-all duration-300 ${
                   isActive
-                    ? "bg-red-500 text-white"
+                    ? "bg-[#EE66A2] text-white"
                     : isCompleted
-                    ? "bg-gray-700 text-white"
+                    ? "bg-[#417ABD] text-white"
                     : "bg-gray-300 text-gray-600"
                 }`}
               >
@@ -39,17 +39,17 @@ export default function StepTracker({ step, petName = "tu mascota" }) {
               </div>
 
               {/* Texto del paso */}
-              <div className="flex flex-col items-center mt-2 text-center">
+              <div className="flex flex-col items-center mt-2 text-center ">
                 <span
-                  className={`text-xs uppercase font-semibold tracking-wide ${
-                    isActive ? "text-red-500" : "text-gray-500"
+                  className={`text-xs uppercase font-semibold font-CeraRoundProLight tracking-wide ${
+                    isActive ? "text-[#EE66A2]" : "text-gray-500"
                   }`}
                 >
                   {s.label}
                 </span>
                 <span
-                  className={`text-sm font-semibold ${
-                    isActive ? "text-red-600" : "text-gray-700"
+                  className={`text-sm font-semibold font-CeraRoundProLight ${
+                    isActive ? "text-[#EE66A2]" : "text-gray-700"
                   }`}
                 >
                   {s.title}
